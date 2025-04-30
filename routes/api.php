e9ba51f5a6a10103686fe53208a01708c4cd16f7
shad arf <sa1121@cs.soran.edu.iq>
2025-05-01 01:28:30 +0300

    add auth user

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,13 @@ Route::get('/items', [ItemController::class, 'index']); // Get all items
 Route::apiResource('feedbacks', controller: FeedbackController::class);
 // Protected Routes (Require JWT Authentication)
 Route::middleware('auth:api')->group(function () {
+
     Route::get('/items/{id}', [ItemController::class, 'show']); // Get a specific item
     Route::post('/items', [ItemController::class, 'store']); // Create an item
     Route::get('/users', action: [AuthController::class, 'userProfile']);
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('/refresh', [AuthController::class, 'refresh'])->name('refresh');
+    Route::post('/changePassword', [AuthController::class, 'changePassword'])->name('changePassword');
     Route::patch('/items/{id}', [ItemController::class, 'update']); // Patch update an item
     Route::delete('/items/{id}', [ItemController::class, 'destroy']); // Delete an item
     // optional route to mark item as taken
